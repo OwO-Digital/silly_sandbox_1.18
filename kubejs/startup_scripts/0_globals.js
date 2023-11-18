ad527728020a49965d151f0fa401bae5ddd7bb89
Octelly <eli@stefek.cz>
2023-11-18 18:52:08 +0100

    updated woodtype list

--- kubejs/startup_scripts/0_globals.js
+++ kubejs/startup_scripts/0_globals.js
@@ -14,6 +14,8 @@ let BOP = (id, x) => MOD("biomesoplenty", id, x)
 let TWB = (id, x) => MOD("wildbackport", id, x)
 let ECO = (id, x) => MOD("ecologics", id, x)
 let ITA = (id, x) => MOD("italian_delight", id, x)
+let CDD = (id, x) => MOD("create_dd", id, x)
+let RU = (id, x) => MOD("regions_unexplored", id, x)
 
 let wood_types = [
     [MC, "oak"],
@@ -22,6 +24,10 @@ let wood_types = [
     [MC, "jungle"],
     [MC, "acacia"],
     [MC, "dark_oak"],
+    [CDD, "rose"],
+    [CDD, "smoked"],
+    [CDD, "spirit"],
+    [CDD, "rubber"],
     [AP, "twisted"],
     [ENV, "willow"],
     [ENV, "cherry"],
@@ -31,10 +37,26 @@ let wood_types = [
     [TIC, "greenheart"],
     [TIC, "skyroot"],
     [TIC, "bloodshroom"],
+    [RU, "baobab"],
+    [RU, "blackwood"],
+    [RU, "cherry"],
+    [RU, "cypress"],
+    [RU, "dead"],
+    [RU, "eucalyptus"],
+    [RU, "joshua"],
+    [RU, "larch"],
+    [RU, "maple"],
+    [RU, "mauve"],
+    [RU, "palm"],
+    [RU, "pine"],
+    [RU, "redwood"],
+    [RU, "willow"],
     [ATM, "rosewood"],
     [ATM, "morado"],
     [ATM, "yucca"],
+    [ATM, "kousa"],
     [ATM, "aspen"],
+    [ATM, "grimwood"],
     [AUT, "maple"],
     [QRK, "blossom"],
     [QRK, "azalea"]
@@ -60,7 +82,7 @@ wood_types.forEach(type => {
 
     // WOOD
     if (type[0] != ITA) {
-        if (type[1].endsWith("wood") && type[0] != BYG && type[0] != BOP) {
+        if (type[1].endsWith("wood") && type[0] != BYG && type[0] != BOP && type[0] != RU) {
             global.wood_types[type].wood = type[0](type[1])
         } else {
             global.wood_types[type].wood = type[0](type[1] + "_wood")
@@ -71,7 +93,7 @@ wood_types.forEach(type => {
     if (type[0] != ITA) {
         if (type[0] == ECO && type[1] == "flowering_azalea") {
             global.wood_types[type].stripped_wood = type[0]("stripped_azalea_wood")
-        } else if (type[1].endsWith("wood") && type[0] != BYG && type[0] != BOP) {
+        } else if (type[1].endsWith("wood") && type[0] != BYG && type[0] != BOP && type[0] != RU) {
             global.wood_types[type].stripped_wood = type[0]("stripped_" + type[1])
         } else {
             global.wood_types[type].stripped_wood = type[0]("stripped_" + type[1] + "_wood")
